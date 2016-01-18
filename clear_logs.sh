ae896f28137ceb5856cf3462687bfb865807ca21
Your Name <you@example.com>
2016-01-18 15:03:38 +0100

    Added clear_logs.sh

---
+++ clear_logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "" > /opt/graphite/storage/log/webapp/info.log
+echo "" > /opt/graphite/storage/log/webapp/exception.log
+echo "" > /opt/graphite/storage/error.log
+echo "" > /opt/graphite/storage/access.log
+
